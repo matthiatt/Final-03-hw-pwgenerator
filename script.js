aba44915578f4bc4580d3ba88a0664cce36f59fc
Matthew Hiatt <mhiatt62@gmail.com>
2020-10-28 02:41:26 -0500

    first edit

--- script.js
+++ script.js
@@ -1,39 +1,68 @@
-//generate random password
-function generate(){
-    //set password length and/or complexity
-    let complexity = document.getElementById("slider").value;
-    //possible password values
-    let values = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890-=+_!@#$%^&*()";
-    let password = "";
-    //create fwd loop to choose password char
-    for(var i = 0; i <= complexity; i++){
-        password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
-    }
-    // add password to textbox/display area
-    document.getElementById("display").value = password;
-    //add password to previously generated passswords section
-    document.getElementById("lastNums").innerHTML += password + "<br />";
-    
-}
-//set default length display by 25
-document.getElementById("length").innerHTML = "Length 25";
-//function to set length based on slider position
-document.getElementById("slider").oninput = function(){
-    
-    if(document.getElementById("slider").value > 0){
-        document.getElementById("length").innerHTML = "Length" + document.getElementById("slider").value;
-    }
-    else{
-        document.getElementById("length").innerHTML = "Length: 1";
-    }
+let minCharLength = 8;
+let maxCharLength = 50;
+let characterNormal = "a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z";
+let characterCaps = "A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q,, R, S, T, U, V, W, X, Y, Z";
+let characterNums = "0, 1, 2, 3, 4, 5, 6, 7, 8, 9";
+let characterSpecial = "!, @, #, $, %, ^, &, *, -,  =, +, _, ?";
+let mainKeyTempPw = "a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z, A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z, 1, 2, 3, 4, 5, 6, 7, 8, 9, !, @, #, $, %, ^, &, *, -, =, +, _, ?";
+let id = "";
+
+// Hoisting const throughout the process of showing and hiding the generated PW. //
+const button = document.getElementById('generate');
+
+function butnListenEvent() {
+    button.addEventListener('click', event => {
+    button.innerHTML = `${event.detail}`;
+    });
 }
 
-//function to copy password clipboard
-function copyPassword(){
+// User clicks button, then to expect a string of random variables based on thier decisions(settings).
+// Calling the variable from line 8.
+function tempPasswordConfig(id) {
+    for (var i = 0; i < 1; i++) {
+        id +=characters.charAt(math.floor(math.random() * charLength));
+    }
+    return id;
+    }
 
-    document.getElementById("display").select();
 
-    document.execCommand("Copy");
+// Creating a function to add extra security to the password.
+// Adding a feature to show and hide the password generated.
+function clickActionDisplay() {
+    var actionDisplayPw = document.getElementById("card-body");
+        if (actionDisplayPw.style.display === "none") {
+            actionDisplayPw.style.display = "block";
+        } else {
+            actionDisplayPw.style.display = "none";
+        } 
 
-    alert("Password copied to clipboard");
+
+$(document).ready(function(){
+  $("#card-body").click(function(clickActionDisplay) {
+    $("p").append(id);
+    return clickActionDisplay();
+  });
+  console.log(clickDisplay);
+  console.log(clickActionDisplay);
+  console.log(display);
+  console.log(document);
+  
+})}
+
+// Add in dbClick feature to copy to clipboard and a button
+
+function generateTempPassword() {
+    password.value = tempPassword(card-body.pwLength.value);
+    return id();
+}
+
+var charCopy = document.getElementById("copy");
+charCopy.addEventListener("click", function () {
+    copyPassword();
+});
+
+function passwordCoppied() {
+    document.getElementById("password").select();
+    document.execCommand("copy");
+    alert("You successfully copied the password to your clipboard.")
 }

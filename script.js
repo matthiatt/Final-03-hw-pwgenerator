45f710f9a7308dfa791f2060069b4b63860781b8
Matthew Hiatt <mhiatt62@gmail.com>
2020-10-31 12:27:08 -0500

    new edit - test

--- script.js
+++ script.js
@@ -1,39 +1,56 @@
 let minCharLength = 8;
 let maxCharLength = 50;
-let characterNormal = "a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z";
-let characterCaps = "A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q,, R, S, T, U, V, W, X, Y, Z";
-let characterNums = "0, 1, 2, 3, 4, 5, 6, 7, 8, 9";
-let characterSpecial = "!, @, #, $, %, ^, &, *, -,  =, +, _, ?";
-let mainKeyTempPw = "a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z, A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z, 1, 2, 3, 4, 5, 6, 7, 8, 9, !, @, #, $, %, ^, &, *, -, =, +, _, ?";
+const characterNormal = ["a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z"];
+const characterCaps = ["A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q,, R, S, T, U, V, W, X, Y, Z"];
+const characterNums = ["0, 1, 2, 3, 4, 5, 6, 7, 8, 9"];
+const characterSpecial = ["!, @, #, $, %, ^, &, *, -,  =, +, _, ?"];
+const mainKeyTempPw = ["a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z, A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z, 1, 2, 3, 4, 5, 6, 7, 8, 9, !, @, #, $, %, ^, &, *, -, =, +, _, ?"];
 let id = "";
 
 // Hoisting const throughout the process of showing and hiding the generated PW. //
+const pwBtn = document.querySelector("generate")
 const button = document.getElementById('generate');
 
 function butnListenEvent() {
     button.addEventListener('click', event => {
     button.innerHTML = `${event.detail}`;
+    userInput();
+    userChoicesArrayGenerator();
+    newGeneratedPw();
+    choicesArr = [];
     });
 }
 
 // User clicks button, then to expect a string of random variables based on thier decisions(settings).
 // Calling the variable from line 8.
-function tempPasswordGenerator(id) {
-    for (var i = 0; i < minCharLength; i++) {
-        var pwvVriables = 
-    }
-    return id;
-    }
+// function tempPasswordGenerator(id) {
+//     for (var i = 0; i < minCharLength; i++) {
+//         var pwvVriables = 
+//     }
+//     return id;
+//     }
 
-function hidePass() {
+function newGeneratedPw() {
+    if (choicesArr.length < typeNumbers.length || getLength < 8) {
+        alert("You did not meet the minimum standard ")
+    } else {
+        for (i = 0; i < getlength; i++) {
+            newPassword += choicesArr[Math.floor(Math.random() * (choicesArr.length - 1))];
+        }
+        displayPasswordEl.textContent = newPassword;
+    }
+    newPassword = [];
+} 
+
+// function hidePass() {
   // Thanks again to Sathanus, on discord, for the suggestion of keeping
   // **NOTE** I am Sathanus on Discord. 
-  var currentDisplayText=document.querySelector("#password");
-  if(currentDisplayText.value.split(" ")[1]!="password")
-    document.querySelector("#password").value = id;
-}
-  document.querySelector("#password").value=id;
-  copyToClipboard();
+//   var currentDisplayText=document.querySelector("#password");
+//   if(currentDisplayText.value.split(" ")[1]!="password")
+//     document.querySelector("#password").value = id;
+// }
+//   document.querySelector("#password").value=id;
+//   copyToClipboard();
 // Creating a function to add extra security to the password.
 // Adding a feature to show and hide the password generated.
 // function clickActionDisplay() {
